74d07c2954e6bbce5afe7465a6e300dbfc106a32
Anton Christoffersson <anton_christoffersson@hotmail.com>
2024-10-05 12:26:27 +0200

    Added flake.nix and co

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              pkgs.tree-sitter
+              pkgs.tree-sitter-grammars.tree-sitter-javascript
+              pkgs.tree-sitter-grammars.tree-sitter-typescript
+              pkgs.nodejs_22
+              (pkgs.python311Packages.python.withPackages (p: [ p.rangehttpserver ]))
+            ];
+            shellHook = '''';
+          };
+      }
+    );
+}
